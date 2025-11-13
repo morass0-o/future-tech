70e389502c4520374a7d62e06a728eacaea429b6
morass0-o <mailforkittens@gmail.com>
2025-11-13 04:24:39 +0300

    First

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+import Header from "./Header.js";
+import TabCollection from "./Tabs.js";
+import VideoPlayerCollection from "./VideoPlayer.js";
+import ExpandableContentCollection from "./ExpandableContent.js";
+import InputMaskCollection from "./InputMask.js";
+import SelectCollection from "./Select.js";
+import defineScrollBarWidthCSSVar from "./utils/defineScrollBarWidthCSSVar.js";
+
+new Header()
+new TabCollection()
+new VideoPlayerCollection()
+new ExpandableContentCollection()
+new InputMaskCollection()
+new SelectCollection()
+
+defineScrollBarWidthCSSVar()
